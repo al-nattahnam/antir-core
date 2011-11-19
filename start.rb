0a6d0757d1ed494838278854c90af2b3073b1159
Fernando Alonso <krakatoa1987@gmail.com>
2011-11-19 07:03:07 -0300

    Updated start file

--- start.rb
+++ start.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'antir-core'
 
-Antir::Core.instance('/home/fernando/desarrollo/workspace/experimentos/antir/core/config.yml')
-Antir::Core.start
+Antir::Core.load_config('/opt/core.yml')
+core = Antir::Core.local
+core.start
